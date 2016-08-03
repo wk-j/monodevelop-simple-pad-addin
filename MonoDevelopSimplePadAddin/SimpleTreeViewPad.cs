827feba86a63ee0cf396081aff04aa70c5641b3b
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-08-04 01:03:02 +0700

    Add test item to tree view

--- MonoDevelopSimplePadAddin/SimpleTreeViewPad.cs
+++ MonoDevelopSimplePadAddin/SimpleTreeViewPad.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using MonoDevelop.Ide;
 using MonoDevelop.Ide.Gui.Components;
 using MonoDevelop.Ide.Gui.Pads;
+using MonoDevelop.Projects;
 
 namespace MonoDevelopSimplePadAddin {
 	public class SimpleTreeViewPad: TreeViewPad {
@@ -10,11 +12,17 @@ namespace MonoDevelopSimplePadAddin {
 		public override void Initialize(NodeBuilder[] builders, TreePadOption[] options, string menuPath) {
 			base.Initialize(builders, options, menuPath);
 
+			//foreach (WorkspaceItem it in IdeApp.Workspace.Items)
+			var it = new TreeViewItem("Hello", Gtk.Stock.New);
+				treeView.AddChild(it);
 
-			TreeView.AddChild("Hello word");
-			TreeView.AddChild("Hello word");
-			TreeView.AddChild("Hello word");
+		}
+		protected virtual void OnOpenWorkspace(object sender, WorkspaceItemEventArgs e) {
+			//treeView.AddChild(e.Item);
 		}
 
+		protected virtual void OnCloseWorkspace(object sender, WorkspaceItemEventArgs e) {
+			//treeView.RemoveChild(e.Item);
+		}
 	}
 }
